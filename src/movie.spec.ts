27c38211e0ea5aff0dc34c991361354e0332ccb1
Josh Medeski <josh.medeski@gmail.com>
2022-01-22 18:51:59 -0600

    feat: add tests with snapshots

--- src/movie.spec.ts
+++ src/movie.spec.ts
@@ -4,7 +4,45 @@ describe("movie", () => {
   const tmdb = new Tmdb()
 
   it("getDetails", async () => {
-    const details = await tmdb.movie.getDetails("603")
-    expect(details).toMatchSnapshot()
+    const result = await tmdb.movie.getDetails("603")
+    expect(result).toMatchSnapshot()
   })
+
+  it.skip("getAccountState", async () => {})
+  it.skip("getAlternativeTitles", async () => {})
+  it.skip("getChanges", async () => {})
+
+  it("getCredits", async () => {
+    const result = await tmdb.movie.getCredits("603")
+    expect(result).toMatchSnapshot()
+  })
+
+  it.skip("getExternalIds", async () => {})
+
+  it("getImages", async () => {
+    const result = await tmdb.movie.getImages("603")
+    expect(result).toMatchSnapshot()
+  })
+
+  it("getKeywords", async () => {
+    const result = await tmdb.movie.getKeywords("603")
+    expect(result).toMatchSnapshot()
+  })
+
+  it.skip("getLists", async () => {})
+
+  it("getRecommendations", async () => {
+    const recommendations = await tmdb.movie.getRecommendations("603")
+    expect(recommendations).toMatchSnapshot()
+  })
+
+  it.skip("getReleaseDates", async () => {})
+  it.skip("getReview", async () => {})
+  it.skip("getSimilarMovies", async () => {})
+  it.skip("getTranslations", async () => {})
+  it.skip("getVideos", async () => {})
+  it.skip("getWatchProviders", async () => {})
+
+  it.skip("rateMovie", async () => {})
+  it.skip("deleteRating", async () => {})
 })
