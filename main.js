c8bb715d534956ba0bb9f431af0d7f22ac96dc48
Josh Medeski <josh.medeski@gmail.com>
2022-01-22 18:13:26 -0600

    feat: add ts-jest

---
+++ main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  stories: [
+    '../app/stories/**/*.stories.mdx',
+    '../app/stories/**/*.stories.@(ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-dark-mode',
+    //'storybook-tailwind-dark-mode'
+    'storybook-addon-tw-dm-toggle'
+  ],
+  framework: '@storybook/react'
+}
