a7c3326ef9ba7c4e6d37423dfc931a66a6029d21
Josh Medeski <josh.medeski@gmail.com>
2022-04-24 07:41:39 -0500

    feat: add /certifiation support

--- src/certifications.ts
+++ src/certifications.ts
@@ -1,6 +1,29 @@
-import { Api, TmdbApiOptions } from "./api"
+import { Api } from "./api"
+import {mockMovieCertifications, MovieCertifications} from "./certifications/movie"
 
 export default class Certifications extends Api {
-  async getMovieCertifications() {}
-  async getTvShowCertifications() {}
+  /**
+   * Get an up to date list of the officially supported movie certifications on TMDB.
+   * @link https://developers.themoviedb.org/3/certifications/get-movie-certifications
+   */
+  async getMovieCertifications() {
+    return await this.get<MovieCertifications>(
+      `/certification/movie/list`,
+      {},
+      mockMovieCertifications,
+    )
+  }
+
+  /**
+   * Get an up to date list of the officially supported movie certifications on TMDB.
+   *
+   * @link https://developers.themoviedb.org/3/certifications/get-movie-certifications
+   */
+  async getTvCertifications() {
+    return await this.get<MovieCertifications>(
+      `/certification/movie/list`,
+      {},
+      mockMovieCertifications,
+    )
+  }
 }
